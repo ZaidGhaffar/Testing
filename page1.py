1d2a46a3e5e572d8204f3abb31937961851557de
Zaid Ghaffar <stemlabs.founder@gmail.com>
2025-03-01 23:55:38 +0500

    adding changings

--- page1.py
+++ page1.py
@@ -1,1 +1,2 @@
 print("A new developers comes in & have added some changes let's us see what changes he/she has made yet")
+print("A new developers comes in & have added some changes let's us see what changes he/she has made yet")
